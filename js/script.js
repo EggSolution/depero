633f735248facff3564d11f15c532523e318850f
s3rgi0s <bertagnollimarco999@gmail.com>
2021-12-14 22:32:18 +0100

    finito le slide che scorrono!!!!!!!!!!!

--- js/script.js
+++ js/script.js
@@ -1,42 +1,43 @@
-import ciao from "../puntiniModule/puntini.js";
-
-allert(ciao);
-
+// import ciao from "../puntiniModule/puntini.js";
 
 window.onload = function(){
     const img = document.getElementById("sfondoScorriId");
-    tempo = 3000;
-    tempo1 = tempo * 1;
-    tempo2 = tempo * 2;
-    tempo3 = tempo * 3;
-    tempo4 = tempo * 4;
-    tempo5 = tempo * 5;
-    tempo6 = tempo * 6;
-    tempo7 = tempo * 7;
-    tempo8 = tempo * 8;
-    tempo9 = tempo * 9;
-    var i = 10;
+    var incrementazione;
+    var posizione = 0;
+    var tempo = 5;
+    tempo *= 1000;
 
-    while(i < 100){
-        setTimeout(cambioSlide1, tempo1)
-        setTimeout(cambioSlide2, tempo2)
-        setTimeout(cambioSlide1, tempo3)
-        setTimeout(cambioSlide2, tempo4)
-        setTimeout(cambioSlide1, tempo5)
-        setTimeout(cambioSlide2, tempo6)
-        setTimeout(cambioSlide1, tempo7)
-        setTimeout(cambioSlide2, tempo8)
-        setTimeout(cambioSlide2, tempo9)
-        i = i + 1;
+    function cambioSlide(direzione) {
+        switch(direzione){
+            case "avanti":
+                incrementazione += 100/3;
+                img.style.transform = String("translate(-"+incrementazione+"%)");
+                posizione += 1;
+                console.log(incrementazione);
+                break;
+            case "indietro":
+                incrementazione = 0;
+                img.style.transform = String("translate(-"+incrementazione+"%)");
+                posizione = 0;
+                console.log(incrementazione);
+                break;
+            default:
+                alert("error 1: la funzione cambio slide non funziona correttamente");
+        }
     }
 
-    function cambioSlide1() {
-        img.style.transform = "translate(-50%)";
-        console.log("1");
+    function cambioAutomatico(){
+        if(posizione == 0){
+            cambioSlide("avanti");
+        }else if(posizione == 1){
+            cambioSlide("avanti");
+        }else if(posizione == 2){
+            cambioSlide("indietro");
+        }else{
+            alert("error 2: la funzione cambio automatico non funziona correttamente");
+        }
     }
-    function cambioSlide2() {
-        img.style.transform = "translate(0)";
-        console.log("2");
-    }
+
+    setInterval(cambioAutomatico, tempo);
 
 };
