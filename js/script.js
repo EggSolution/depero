402371e80705306d126a8a5e11c7bab42a5c7cf8
s3rgi0s <bertagnollimarco999@gmail.com>
2021-12-18 17:54:39 +0100

    fatto anche i bottoni e finito la parteslide

--- js/script.js
+++ js/script.js
@@ -1,16 +1,23 @@
 // import ciao from "../puntiniModule/puntini.js";
 
 window.onload = function(){
-    const img = document.getElementById("sfondoScorriId");
-    var incrementazione;
+    const img = document.querySelector("#sfondoScorriId");
+    const bottoneSlide1 = document.querySelector(".bottoneSlide1");
+    const bottoneSlide2 = document.querySelector(".bottoneSlide2");
+    const bottoneSlide3 = document.querySelector(".bottoneSlide3");
+    var incrementazione = 0;
     var posizione = 0;
-    var tempo = 5;
+    var tempo = 8;
     tempo *= 1000;
 
-    function cambioSlide(direzione) {
+    window.cambioSlide=function(direzione) {
         switch(direzione){
             case "avanti":
-                incrementazione += 100/3;
+                if(posizione === 0){
+                    incrementazione = 100/3;
+                } else if (posizione === 1){
+                    incrementazione = (100/3)*2;
+                }
                 img.style.transform = String("translate(-"+incrementazione+"%)");
                 posizione += 1;
                 console.log(incrementazione);
@@ -21,23 +28,51 @@ window.onload = function(){
                 posizione = 0;
                 console.log(incrementazione);
                 break;
+            case "uno":
+                incrementazione = 0;
+                img.style.transform = String("translate(-"+incrementazione+"%)");
+                incrementazione = 0;
+                posizione = 0;
+                break;
+            case "due":
+                incrementazione = 100/3;
+                img.style.transform = String("translate(-"+incrementazione+"%)");
+                incrementazione = 0;
+                posizione = 1;
+                break;
+            case "tre":
+                incrementazione = (100/3)*2;
+                img.style.transform = String("translate(-"+incrementazione+"%)");
+                incrementazione = 0;
+                posizione = 2;
+                break;
             default:
-                alert("error 1: la funzione cambio slide non funziona correttamente");
+                alert("error 1: la funzione cambioSlide non funziona correttamente");
         }
     }
 
     function cambioAutomatico(){
-        if(posizione == 0){
-            cambioSlide("avanti");
-        }else if(posizione == 1){
-            cambioSlide("avanti");
-        }else if(posizione == 2){
-            cambioSlide("indietro");
-        }else{
-            alert("error 2: la funzione cambio automatico non funziona correttamente");
+
+        switch(posizione){
+            case 0:
+                cambioSlide("avanti");
+                break;
+            case 1:
+                cambioSlide("avanti");
+                break;
+            case 2:
+                cambioSlide("indietro");
+                break;
+            default:
+                alert("error 2: la funzione cambioAutomatico non funziona correttamente");
         }
+        console.log(posizione);
     }
 
-    setInterval(cambioAutomatico, tempo);
+    // cambioSlide(0)
+    // cambioSlide(1)
+    // cambioSlide(2)
 
+
+    setInterval(cambioAutomatico, tempo);
 };
