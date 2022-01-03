7f67ffb387e87606177aa1833080cc35754ab0af
s3rgi0s <marco2006bertagnolli@gmail.com>
2022-01-03 13:36:42 +0100

    attemp 3 redirect to home button

--- js/script404.js
+++ js/script404.js
@@ -1,6 +1,4 @@
 function redirectHome(){
-    console.log(window.location.pathname);
-    var realLink = window.location.href.replace(window.location.pathname, "");
-    console.log(realLink);
-    //window.location.replace(realLink);
+    var realLink = window.location.href.replace(window.location.pathname, "/");
+    window.location.replace(realLink);
 }
