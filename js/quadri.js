3b669e91ba43ade153016974eb52dc02d9f30e63
maxglio <massimo.maglio.06@gmail.com>
2022-01-12 22:40:39 +0100

    inizio sezione quadri

---
+++ js/quadri.js
@@ -0,0 +1,6 @@
+window.onload = function(){
+    var body = document.querySelector ("#body");
+    var height = window.innerHeight
+    console.log (height)
+    body.style.height = height + "px";
+} 
