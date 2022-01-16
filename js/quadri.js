f4994eb7d08b28b3e1507bed118cf76801041e0c
s3rgi0s <marco2006bertagnolli@gmail.com>
2022-01-16 13:27:15 +0100

    fixato le percentuali

--- js/quadri.js
+++ js/quadri.js
@@ -2,15 +2,8 @@ window.onload = function(){
 //variabili
     //variabili elementi
     var home = document.querySelector ("#home");
-    var sezioneProva = document.querySelector ("#sezioneProva");
-    var titoloSezione = document.querySelector ("#titoloSezione");
-    var corpoSezione = document.querySelector ("#corpoSezione");
-    //variabili altezze
+    // variabili altezze
     var altezza100 = window.innerHeight;
-    var altezza50 = altezza100/2;
-    var altezza10_50 = (altezza50/100)*25;
 //codice
     home.style.height = altezza100+ "px";
-    sezioneProva.style.height = altezza50 + "px";
-    titoloSezione.style.height = altezza10_50 + "px";
 } 
