5a7bcd5818fbde306cc181f368458adc43e4ae42
maxglio <massimo.maglio.06@gmail.com>
2022-01-23 19:38:46 +0100

    riconosce quali sezioni vogliono il titolo e quali no

--- js/quadri.js
+++ js/quadri.js
@@ -9,51 +9,6 @@ window.onload = function(){
     // variabile n° sezioni
     var Nsezioni = 0;
 //codice
-//FUNZIONE TITOLI
-    function titoloQuadri(titolo,descrizione){
-        Nsezioni += 1;
-        //TITOLO QUADRO
-        eval("var titoloQuadro" + Nsezioni + "= document.createElement ('div');");
-        eval("document.querySelector(#sezioneQuadri).appendchild (titoloQuadro" + Nsezioni +");");
-        eval("titoloQuadro" + Nsezioni + ".textContent = ';");
-        eval("titoloQuadro" + Nsezioni + ".className = 'titoloQuadro';");
-        // SEZIONE TITOLO
-        eval("var sezioneTitolo-2 = document.createElement ('div');");
-        eval("titoloQuadro" + Nsezioni + ".appendChild (sezioneTitolo-2);");
-        eval("sezioneTitolo-2.textContent = '';");
-        eval("sezioneTitolo-2.className = 'sezioneTitolo-2';");
-        //TITOLO
-        eval("var titolo-2 = document.createElement ('div');");
-        eval("sezioneTitolo-2.appendChild (titolo-2);");
-        eval("titolo-2.textContent ='" + titolo + "';")
-        eval("titolo-2.className = 'titolo-2';");
-        //SCRITTE TITOLO
-        eval("var scritteTitolo-2 = document.createElement ('a');");
-        eval("titolo-2.appendChild (scritteTitolo-2);");
-        eval("scritte-titolo-2.textContent = '';");
-        eval("scritteTitolo-2.className = 'scritteTitolo-2';");
-        //RIGA TITOLO
-        eval("var rigaTitolo-2 = document.createElment ('div');");
-        eval("sezioneTitolo-2.appendChild (rigaTitolo-2);");
-        eval("rigaTitolo-2.textContent = '';");
-        eval("rigaTitolo-2.className = 'rigaTitolo-2';");
-        // CORPO QUADRI
-        eval("var corpoQuadri-2 = document.createElement ('div');");
-        eval("titoloQuadro" + Nsezioni + ".appendChild (corpoQuadri-2);");
-        eval("corpoQuadri-2.textContent = '';");
-        eval("corpoQuadri-2.className = 'corpoQuadri-2';");
-        //DIV SCRITTE
-        eval("var divScritte-2 = document.createElement ('div');");
-        eval("corpoQuadri-2.appendchild (divScritte-2);");
-        eval("divScritte-2.textContent = '';");
-        eval("divScritte-2.className = 'divScritte-2';")
-        // SCRITTE CORPO QUADRI
-        eval("var scritteCorpoQuadri-2 = document.createElement ('a');");
-        eval("divScritte-2.appendChild (scritteCorpoQuadri-2);");
-        eval("scritteCorpoQuadri-2.textContent ='" + descrizione + "';");
-        eval("scritteCorpoQuadri-2.className = 'scritteCorpoQuadri-2';");
-    }
-    titoloQuadri("titolo","chhkhkhjkjh")
 //FUNZIONE QUADRI
     function sezioniQuadri(titolo1, testo, immagine, appartenenza){
         Nsezioni += 1;
@@ -138,11 +93,23 @@ window.onload = function(){
         else{
             corpoSezione.style.flexDirection = "row-reverse";
         }
+
+        //VERIFICARE SE VUOLE IL TITOLO O NO
+        if (appartenenza == "true"){
+            console.log ("gg");
+        }
+        else if(appartenenza == "false"){
+            console.log("benissimo");
+             // SEZIONE
+        }
+        else{
+            console.log("not gg");
+        }
         
     }
 // funzione         titolo       testo        Nomeimmagine.Estensione
-    sezioniQuadri ("Titolo","Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", "quadro-prova.jpg")
-    sezioniQuadri ("Titolo","Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", "quadro-prova.jpg")
-    sezioniQuadri ("Titolo","Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", "quadro-prova.jpg")
-    sezioniQuadri ("Titolo","Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", "quadro-prova.jpg")
+    sezioniQuadri ("Titolo","", "quadro-prova.jpg","true")
+    sezioniQuadri ("Titolo","", "quadro-prova.jpg","false")
+    sezioniQuadri ("Titolo","", "quadro-prova.jpg","true")
+
 }
